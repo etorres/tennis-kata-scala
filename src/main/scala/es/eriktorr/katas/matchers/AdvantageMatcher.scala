b51b2ef1117bbaa4bcb0d3f5ce873240320f8dcf
Erik Torres <erik.torres@codurance.com>
2019-09-24 07:33:52 +0200

    Clean-up matchers

--- src/main/scala/es/eriktorr/katas/matchers/AdvantageMatcher.scala
+++ src/main/scala/es/eriktorr/katas/matchers/AdvantageMatcher.scala
@@ -5,11 +5,20 @@ import es.eriktorr.katas.ScoreMatcher
 object AdvantageMatcher extends ScoreMatcher {
 
   override def matches(player1Points: Int, player2Points: Int): Boolean = {
-    player1Points >= 3 && player2Points >= 3 && math.abs(player1Points - player2Points) == 1
+    hasAtLeastThreePoints(player1Points) && hasAtLeastThreePoints(player2Points) &&
+      oneMorePointThanTheOpponent(player1Points, player2Points)
   }
 
   override def tennisScoreFrom(player1Points: Int, player2Points: Int): String = {
     if (player1Points > player2Points) "advantage player 1" else "advantage player 2"
+  }
+
+  private def oneMorePointThanTheOpponent(player1Points: Int, player2Points: Int) = {
+    math.abs(player1Points - player2Points) == 1
+  }
+
+  private def hasAtLeastThreePoints(player1Points: Int) = {
+    player1Points >= 3
   }
 
 }
--- src/main/scala/es/eriktorr/katas/matchers/DeuceMatcher.scala
+++ src/main/scala/es/eriktorr/katas/matchers/DeuceMatcher.scala
@@ -5,11 +5,20 @@ import es.eriktorr.katas.ScoreMatcher
 object DeuceMatcher extends ScoreMatcher {
 
   override def matches(player1Points: Int, player2Points: Int): Boolean = {
-    player1Points >= 3 && player2Points >= 3 && player1Points == player2Points
+    hasAtLeastThreePoints(player1Points) && hasAtLeastThreePoints(player2Points) &&
+      scoresAreEqual(player1Points, player2Points)
   }
 
   override def tennisScoreFrom(player1Points: Int, player2Points: Int): String = {
     "deuce"
+  }
+
+  private def scoresAreEqual(player1Points: Int, player2Points: Int) = {
+    player1Points == player2Points
+  }
+
+  private def hasAtLeastThreePoints(player1Points: Int) = {
+    player1Points >= 3
   }
 
 }
